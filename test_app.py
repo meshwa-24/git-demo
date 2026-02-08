1bdfd31359cacb262cbcec62f809efc04caf913c
meshwa-24 <23bt04071@gsfcuniversity.ac.in>
2026-02-09 00:29:57 +0530

    Refactor test_app.py for consistency and clarity

--- test_app.py
+++ test_app.py
@@ -1,30 +1,22 @@
-import pytest
-from app import add,subtract,multiply,divide
+from app import add, subtract, multiply, divide
+
 
 def test_add():
-    assert add(2,3)==5
-    assert add(-1,1)==0
-    print("Add test Passed")
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
 
 def test_subtract():
-    assert subtract(5,2)==3
-    assert subtract(10,10)==0
-    print("Sub test Passed")
+    assert subtract(5, 2) == 3
+    assert subtract(10, 10) == 0
+
 
 def test_multiply():
-    assert multiply(3,4)==12
-    assert multiply(0,5)==0
-    print("Multiply test Passed")
+    assert multiply(3, 4) == 12
+    assert multiply(0, 5) == 0
+
 
 def test_divide():
-    assert divide(10,2)==5
-    assert divide(0,5)==0
-    assert divide(5,0)=="Cant divide by zero"
-    print("divide test Passed")
-
-if __name__ = "__main__":
-    test_add()
-    test_subtract()
-    test_multiply()
-    test_divide()
-print("\n All Tests Passed")
+    assert divide(10, 2) == 5
+    assert divide(0, 5) == 0
+    assert divide(5, 0) == "Cant divide by zero"
